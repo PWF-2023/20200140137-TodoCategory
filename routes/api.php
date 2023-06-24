6bf43a51ebb0955827f0d545e18d87554658c960
AnditaKhoiriah <andita.khoiriah.ft20@mail.umy.ac.id>
2023-06-24 10:14:41 +0700

    api:create, update, delete data

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\API\ProfileController;
 use App\Http\Controllers\API\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::middleware('auth:sanctum')->group(function(){Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
     //Route::resource('/todo', TodoController::class);
     Route::apiResource('/todo', TodoController::class);
+    // Route::apiResource('/category', categorycontroller::class);
+    Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
+    Route::delete('/todo', [TodoController::class, 'deleteAllCompleted'])->name('todo.deleteallcompleted');
 });
---
+++ tugas12.md
@@ -0,0 +1,12 @@
+# Tugas 11
+
+## 1. Login
+![Alt text](screenshot/tugas12/Login.png)
+## 2. Category Index
+![Alt text](screenshot/tugas12/Categoryindex.png)
+## 3. Todo Store
+![Alt text](screenshot/tugas12/Todostore.png)
+## 4. Todo Update
+![Alt text](screenshot/tugas12/Todoupdate.png)
+## 5. Todo Delete
+![Alt text](screenshot/tugas12/Tododelete.png)
